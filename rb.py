614e361e85477c59e79d9fa51914e128844b9c87
Lacey <65921622+gemhue@users.noreply.github.com>
2024-09-25 03:13:58 +0000

    Rewrite in Progress

--- rb.py
+++ rb.py
@@ -9,7 +9,8 @@ from datetime import datetime, timezone, timedelta
 bot = commands.Bot(
     command_prefix='rb!',
     description="A multi-purpose Discord bot made by GitHub user gemhue.",
-    intents=discord.Intents.all())
+    intents=discord.Intents.all()
+)
 
 guilds = {}
 
@@ -22,6 +23,7 @@ class ChannelsSelector(ChannelSelect):
             max_values=25,
             row=1
         )
+
     async def callback(self, interaction: discord.Interaction) -> Any:
         channels: list[DropdownView] = self.values
         self.view.values = [c for c in channels]
@@ -34,13 +36,13 @@ class DropdownView(discord.ui.View):
 
     @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green, row=2)
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.value = True
         await interaction.response.defer(thinking=True, ephemeral=True)
         channels = [c.resolve() for c in self.values]
         channelment = [c.mention for c in channels]
         channellist = ", ".join(channelment)
         embed = discord.Embed(title="Selected Channels:", description=f'{channellist}')
         await interaction.followup.send(embed=embed, ephemeral=True)
+        self.value = True
         self.stop()
 
     @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey, row=2)
@@ -87,6 +89,7 @@ class BackgroundTasks(commands.Cog):
         moji8 = bot.get_emoji(1274435528883961989)
         if any(x in message for x in list8):
             await message.add_reaction(moji8)
+        await bot.process_commands(message)
         
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
@@ -120,13 +123,35 @@ class SetupCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(
+        name="Sync",
+        description="Syncs the local command tree (bot owner only)."
+    )
     @commands.is_owner()
     async def sync(self, ctx: commands.Context):
-        await bot.tree.sync()
-        await ctx.send()
+        await ctx.defer(thinking=True)
+        guild = ctx.guild
+        bot.tree.clear_commands(guild=guild)
+        await bot.tree.sync(guild=guild)
+        embed = discord.Embed(title="Update", description=f"The bot's local command tree has been synced!")
+        await ctx.send(embed=embed)
+    
+    @commands.command(
+        name="Global Sync",
+        description="Syncs the global command tree (bot owner only)."
+    )
+    @commands.is_owner()
+    async def globalsync(self, ctx: commands.Context):
+        await ctx.defer(thinking=True)
+        bot.tree.clear_commands(guild=None)
+        await bot.tree.sync(guild=None)
+        embed = discord.Embed(title="Update", description=f"The bot's global command tree has been synced!")
+        await ctx.send(embed=embed)
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(
+            name="Set Channels",
+            description="Set the channels for logging messages, welcome messages, and goodbye messages."
+    )
     @commands.has_guild_permissions(administrator=True)
     async def setchannels(self, ctx: commands.Context, logging_channel: Optional[discord.TextChannel], welcome_channel: Optional[discord.TextChannel], goodbye_channel: Optional[discord.TextChannel]):
         guild = ctx.guild
@@ -135,23 +160,32 @@ class SetupCommands(commands.Cog):
         guilds[guild.id]["goodbye"] = goodbye_channel
         await ctx.send(f"**Logging Channel**: {logging_channel}\n**Welcome Channel**: {welcome_channel}\n**Goodbye Channel**: {goodbye_channel}")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(
+            name="Set Welcome Message",
+            description="Sets the welcome message for members who join the server."
+    )
     @commands.has_guild_permissions(administrator=True)
     async def setwelcome(self, ctx: commands.Context, message: str):
         guild = ctx.guild
         guilds[guild.id]["welcome message"] = message
         await ctx.send(f"**Welcome Message**: {message}")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(
+            name="Set Goodbye Message",
+            description="Sets the goodbye message for members who leave the server."
+    )
     @commands.has_guild_permissions(administrator=True)
     async def setgoodbye(self, ctx: commands.Context, message: str):
         guild = ctx.guild
         guilds[guild.id]["goodbye message"] = message
         await ctx.send(f"**Goodbye Message**: {message}")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(
+            name="Set Join Roles",
+            description="Sets the roles to give to new members who join the server."
+    )
     @commands.has_guild_permissions(administrator=True)
-    async def setjoinrole(self, ctx: commands.Context, role: discord.Role, botrole: Optional[discord.Role]):
+    async def setjoinroles(self, ctx: commands.Context, role: discord.Role, botrole: Optional[discord.Role]):
         guild = ctx.guild
         guilds[guild.id]["join role"] = role
         await ctx.send(f"**Join Role**: {role}")
@@ -159,7 +193,10 @@ class SetupCommands(commands.Cog):
             guilds[guild.id]["bot role"] = botrole
             await ctx.send(f"**Bot Role**: {botrole}")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(
+            name="Activity Roles",
+            description="Assigns an active role to active members and an inactive role to inactive members."
+    )
     @commands.has_guild_permissions(administrator=True)
     async def activityroles(self, ctx: commands.Context, days: int, active: discord.Role, inactive: discord.Role):
         await ctx.defer(ephemeral=True)
@@ -201,10 +238,10 @@ class PurgeCommands(commands.Cog):
 
     @commands.hybrid_command()
     @commands.has_guild_permissions(administrator=True)
-    async def purgeself(self, ctx: commands.Context):
+    async def purgemember(self, ctx: commands.Context, member: discord.Member):
         await ctx.defer(thinking=True, ephemeral=True)
         view = DropdownView()
-        await ctx.send("Which channel(s) would you like to purge messages from?", view=view, ephemeral=True)
+        await ctx.send(f"Which channel(s) would you like to purge {member.mention}'s messages from?", view=view, ephemeral=True)
         await view.wait()
         if view.value == True:
             purgechannels = [c for c in view.values]
@@ -213,7 +250,7 @@ class PurgeCommands(commands.Cog):
                 unpinned = [m for m in messages if not m.pinned]
                 deleted = []
                 while len(unpinned) > 0:
-                    deleted += await channel.purge(check=lambda message: message.author == ctx.user and message.pinned == False, oldest_first=True)
+                    deleted += await channel.purge(check=lambda message: message.author == member and message.pinned == False, oldest_first=True)
                 if len(deleted) == 1:
                     embed = discord.Embed(title="✔️ Success ✔️", description=f'{len(deleted)} message was just removed from {channel.mention}!')
                     await ctx.send(embed=embed, ephemeral=True)
@@ -232,7 +269,7 @@ class PurgeCommands(commands.Cog):
     async def purgechannels(self, ctx: commands.Context):
         await ctx.defer(thinking=True, ephemeral=True)
         view = DropdownView()
-        await ctx.send("Which channel(s) would you like to purge messages from?", view=view, ephemeral=True)
+        await ctx.send("Which channel(s) would you like to purge all unpinned messages from?", view=view, ephemeral=True)
         await view.wait()
         if view.value == True:
             purgechannels = [c for c in view.values]
@@ -292,6 +329,7 @@ class AwardCommands(commands.Cog):
         self.bot = bot
     
     @commands.hybrid_command()
+    @commands.has_guild_permissions(administrator=True)
     async def setawards(self, ctx: commands.Context, name_singular: str, name_plural: str, emoji: str):
         await ctx.defer(thinking=True, ephemeral=True)
         guild = ctx.guild
@@ -310,6 +348,7 @@ class AwardCommands(commands.Cog):
         await ctx.send(embed=embed, ephemeral=True)
 
     @commands.hybrid_command()
+    @commands.has_guild_permissions(administrator=True)
     async def clearawards(self, ctx: commands.Context):
         await ctx.defer(thinking=True)
         guild = ctx.guild
@@ -321,49 +360,57 @@ class AwardCommands(commands.Cog):
         guilds[guild.id] = {}
         embed = discord.Embed(title=f"{moji} {plur_cap} Cleared {moji}", description=f"{guild.name} has had all its {plur_low} cleared!")
         await ctx.send(embed=embed)
+    
+    @commands.hybrid_command()
+    @commands.has_guild_permissions(administrator=True)
+    async def awardreactiontoggle(self, ctx: commands.Context, toggle: bool):
+        guild = ctx.guild
+        guilds[guild.id]["award_react_toggle"] = toggle
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: discord.Member):
         message = reaction.message
         guild = message.guild
-        member = message.author
-        sing_low = guilds[guild.id]["singular_lower"] or "award"
-        sing_cap = guilds[guild.id]["singular_caps"] or "Award"
-        plur_low = guilds[guild.id]["plural_lower"] or "awards"
-        #plur_cap = guilds[guild.id]["plural_caps"] or "Awards"
-        moji = guilds[guild.id]["emoji"] or "🏅"
-        if str(reaction.emoji) == moji:
-            if guild.id in guilds:
-                if member.id in guilds[guild.id]:
-                    guilds[guild.id][member.id] += 1
-                    embed = discord.Embed(title=f"{moji} {sing_cap} Added {moji}", description=f"{user.mention} has added 1 {sing_low} to {member.mention}'s total via an emoji reaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
-                    await message.channel.send(embed=embed, reference=message)
+        if guilds[guild.id]["award_react_toggle"] == True:
+            member = message.author
+            sing_low = guilds[guild.id]["singular_lower"] or "award"
+            sing_cap = guilds[guild.id]["singular_caps"] or "Award"
+            plur_low = guilds[guild.id]["plural_lower"] or "awards"
+            #plur_cap = guilds[guild.id]["plural_caps"] or "Awards"
+            moji = guilds[guild.id]["emoji"] or "🏅"
+            if str(reaction.emoji) == moji:
+                if guild.id in guilds:
+                    if member.id in guilds[guild.id]:
+                        guilds[guild.id][member.id] += 1
+                        embed = discord.Embed(title=f"{moji} {sing_cap} Added {moji}", description=f"{user.mention} has added 1 {sing_low} to {member.mention}'s total via an emoji reaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
+                        await message.channel.send(embed=embed, reference=message)
+                    else:
+                        guilds[guild.id][member.id] = 1
+                        embed = discord.Embed(title=f"{moji} {sing_cap} Added {moji}", description=f"{user.mention} has added 1 {sing_low} to {member.mention}'s total via an emoji reaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
+                        await message.channel.send(embed=embed, reference=message)
                 else:
+                    guilds[guild.id] = {}
                     guilds[guild.id][member.id] = 1
                     embed = discord.Embed(title=f"{moji} {sing_cap} Added {moji}", description=f"{user.mention} has added 1 {sing_low} to {member.mention}'s total via an emoji reaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
                     await message.channel.send(embed=embed, reference=message)
-            else:
-                guilds[guild.id] = {}
-                guilds[guild.id][member.id] = 1
-                embed = discord.Embed(title=f"{moji} {sing_cap} Added {moji}", description=f"{user.mention} has added 1 {sing_low} to {member.mention}'s total via an emoji reaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
-                await message.channel.send(embed=embed, reference=message)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction: discord.Reaction, user: discord.Member):
         message = reaction.message
         guild = message.guild
-        member = message.author
-        sing_low = guilds[guild.id]["singular_lower"] or "award"
-        sing_cap = guilds[guild.id]["singular_caps"] or "Award"
-        plur_low = guilds[guild.id]["plural_lower"] or "awards"
-        #plur_cap = guilds[guild.id]["plural_caps"] or "Awards"
-        moji = guilds[guild.id]["emoji"] or "🏅"
-        if str(reaction.emoji) == moji:
-            if guild.id in guilds:
-                if member.id in guilds[guild.id] and guilds[guild.id][member.id] >= 1:
-                    guilds[guild.id][member.id] -= 1
-                    embed = discord.Embed(title=f"{moji} {sing_cap} Removed {moji}", description=f"{user.mention} has removed 1 {sing_low} from {member.mention}'s total via an emoji unreaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
-                    await message.channel.send(embed=embed, reference=message)
+        if guilds[guild.id]["award_react_toggle"] == True:
+            member = message.author
+            sing_low = guilds[guild.id]["singular_lower"] or "award"
+            sing_cap = guilds[guild.id]["singular_caps"] or "Award"
+            plur_low = guilds[guild.id]["plural_lower"] or "awards"
+            #plur_cap = guilds[guild.id]["plural_caps"] or "Awards"
+            moji = guilds[guild.id]["emoji"] or "🏅"
+            if str(reaction.emoji) == moji:
+                if guild.id in guilds:
+                    if member.id in guilds[guild.id] and guilds[guild.id][member.id] >= 1:
+                        guilds[guild.id][member.id] -= 1
+                        embed = discord.Embed(title=f"{moji} {sing_cap} Removed {moji}", description=f"{user.mention} has removed 1 {sing_low} from {member.mention}'s total via an emoji unreaction. {member.mention}'s new total number of {plur_low} is {guilds[guild.id][member.id]}!")
+                        await message.channel.send(embed=embed, reference=message)
 
     @commands.hybrid_command()
     async def addaward(self, ctx: commands.Context, amount: Optional[int] = None, member: Optional[discord.Member] = None):
@@ -491,12 +538,12 @@ class AwardCommands(commands.Cog):
             await ctx.send(embed=embed, ephemeral=True)
 
 async def setup(bot):
-    await bot.add_cog(BackgroundTasks(bot))
-    await bot.add_cog(SetupCommands(bot))
-    await bot.add_cog(PurgeCommands(bot))
-    await bot.add_cog(AwardCommands(bot))
+    await bot.add_cog(BackgroundTasks(bot), override=True)
+    await bot.add_cog(SetupCommands(bot), override=True)
+    await bot.add_cog(PurgeCommands(bot), override=True)
+    await bot.add_cog(AwardCommands(bot), override=True)
 
-@bot.event()
+@bot.event
 async def on_ready():
     await setup()
     print(f'Logged in as {bot.user}! (ID: {bot.user.id})')
