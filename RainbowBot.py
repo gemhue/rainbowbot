276e11eed1add0553001b4734b472f44574fc734
gemhue <laceyjoms@gmail.com>
2024-10-04 16:42:29 -0500

    Updates

--- RainbowBot.py
+++ RainbowBot.py
@@ -53,44 +53,44 @@ class BackgroundTasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener(name="emoji_reactions")
+    @commands.Cog.listener(name="on_message")
     async def on_message(self, message: discord.Message):
-        message = message.content.lower()
+        messagecont = message.content.lower()
         list1 = ['lesbian','sapphic','wlw']
         moji1 = bot.get_emoji(1274435288499884094)
-        if any(x in message for x in list1):
+        if any(x in messagecont for x in list1):
             await message.add_reaction(moji1)
         list2 = ['gay','achillean','mlm']
         moji2 = bot.get_emoji(1274435330174615624)
-        if any(x in message for x in list2):
+        if any(x in messagecont for x in list2):
             await message.add_reaction(moji2)
         list3 = ['bisexual','biromantic','bi woman','bi women','bi lady','bi ladies','bi girl','bi gal','bi man','bi men','bi guy','bi dude','bi boy','bi person','bi people']
         moji3 = bot.get_emoji(1274435359878676560)
-        if any(x in message for x in list3):
+        if any(x in messagecont for x in list3):
             await message.add_reaction(moji3)
         list4 = ['asexual','aromantic','acespec','arospec','ace spec','aro spec','ace-spec','aro-spec']
         moji4 = bot.get_emoji(1274435406804291634)
-        if any(x in message for x in list4):
+        if any(x in messagecont for x in list4):
             await message.add_reaction(moji4)
         list5 = ['transgender','transsexual','trans woman','trans women','trans lady','trans ladies','trans girl','trans gal','trans man','trans men','trans guy','trans dude','trans boy','trans person','trans people']
         moji5 = bot.get_emoji(1274435448726622208)
-        if any(x in message for x in list5):
+        if any(x in messagecont for x in list5):
             await message.add_reaction(moji5)
         list6 = ['nonbinary','non binary','non-binary','enby']
         moji6 = bot.get_emoji(1274435483912638515)
-        if any(x in message for x in list6):
+        if any(x in messagecont for x in list6):
             await message.add_reaction(moji6)
         list7 = ['transfeminine','trans feminine','trans-feminine','transfem','trans fem','trans-fem']
         moji7 = bot.get_emoji(1274435557744840820)
-        if any(x in message for x in list7):
+        if any(x in messagecont for x in list7):
             await message.add_reaction(moji7)
         list8 = ['transmasculine','trans masculine','trans-masculine','transmasc','trans masc','trans-masc']
         moji8 = bot.get_emoji(1274435528883961989)
-        if any(x in message for x in list8):
+        if any(x in messagecont for x in list8):
             await message.add_reaction(moji8)
         await bot.process_commands(message)
         
-    @commands.Cog.listener(name="member_join")
+    @commands.Cog.listener(name="on_member_join")
     async def on_member_join(self, member: discord.Member):
         guild = member.guild
         guild_id = guild.id
@@ -128,7 +128,7 @@ class BackgroundTasks(commands.Cog):
             botrole = guild.get_role(botrole_id)
             await member.add_roles(botrole)
 
-    @commands.Cog.listener(name="member_remove")
+    @commands.Cog.listener(name="on_member_remove")
     async def on_member_remove(self, member: discord.Member):
         guild = member.guild
         guild_id = guild.id
@@ -2161,8 +2161,18 @@ class RSSFeeds(commands.Cog):
                         link = await self.parsefeed(url, feed_url)
                         if link is not None:
                             async with aiohttp.ClientSession() as session:
-                                webhook = Webhook.from_url(url=url, session=session)
-                                await webhook.send(link)
+                                partialwebhook = Webhook.from_url(url=url, session=session)
+                                webhook = await partialwebhook.fetch()
+                                channel = webhook.channel
+                                messages = [message.content async for message in channel.history(limit=100)]
+                                if link not in messages:
+                                    await webhook.send(link)
+                                else:
+                                    return
+                        else:
+                            return
+                else:
+                    return
             await db.commit()
             await db.close()
 
@@ -2232,4 +2242,5 @@ async def on_ready():
     await setup(bot)
     print(f'Logged in as {bot.user}! (ID: {bot.user.id})')
 
-bot.run('token')
+token = 'token'
+bot.run(token)
