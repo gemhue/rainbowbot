2869b8c7f3d2c918163562d5d361d1f8e716cb13
gemhue <laceyjoms@gmail.com>
2024-10-20 20:49:29 -0500

    Updating: BackgroundTasks

--- cogs/BackgroundTasks.py
+++ cogs/BackgroundTasks.py
@@ -45,65 +45,67 @@ class BackgroundTasks(commands.Cog):
         
     @commands.Cog.listener(name="on_member_join")
     async def on_member_join(self, member: discord.Member):
-        guild = member.guild
-        guild_id = guild.id
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = member.guild
+            guild_id = guild.id
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             cur = await db.execute("SELECT welcome_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             channel_id = row[0]
-            cur = await db.execute("SELECT welcome_message FROM guilds WHERE guild_id = ?", (guild_id,))
-            row = await cur.fetchone()
-            message = row[0]
+            if channel_id is not None:
+                cur = await db.execute("SELECT welcome_message FROM guilds WHERE guild_id = ?", (guild_id,))
+                row = await cur.fetchone()
+                message = row[0]
+                if message is None:
+                    message = f"Welcome to {guild.name}, {member.mention}!"
+                color = member.accent_color
+                time = datetime.now()
+                embed = discord.Embed(color=color, description=message, timestamp=time)
+                avatar = member.display_avatar
+                embed.set_thumbnail(url=avatar)
+                channel = guild.get_channel(channel_id)
+                content = f"-# {member.mention}"
+                await channel.send(content=content, embed=embed)
             cur = await db.execute("SELECT join_role_id FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             role_id = row[0]
+            if role_id is not None and not member.bot:
+                role = guild.get_role(role_id)
+                await member.add_roles(role)
             cur = await db.execute("SELECT bot_join_role_id FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             botrole_id = row[0]
+            if botrole_id is not None and member.bot:
+                botrole = guild.get_role(botrole_id)
+                await member.add_roles(botrole)
             await db.commit()
             await db.close()
-        if channel_id is not None:
-            channel = guild.get_channel(channel_id)
-            if message is not None:
-                embed = discord.Embed(color=member.accent_color, description=message, timestamp=datetime.now())
-                content = f"-# {member.mention}"
-                await channel.send(content=content, embed=embed)
-            else:
-                description = f"Welcome to {guild.name}, {member.mention}!"
-                embed = discord.Embed(color=member.accent_color, description=description, timestamp=datetime.now())
-                content = f"-# {member.mention}"
-                await channel.send(content=content, embed=embed)
-        if role_id is not None and not member.bot:
-            role = guild.get_role(role_id)
-            await member.add_roles(role)
-        if botrole_id is not None and member.bot:
-            botrole = guild.get_role(botrole_id)
-            await member.add_roles(botrole)
 
     @commands.Cog.listener(name="on_member_remove")
     async def on_member_remove(self, member: discord.Member):
-        guild = member.guild
-        guild_id = guild.id
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = member.guild
+            guild_id = guild.id
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             cur = await db.execute("SELECT goodbye_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             channel_id = row[0]
-            cur = await db.execute("SELECT goodbye_message FROM guilds WHERE guild_id = ?", (guild_id,))
-            row = await cur.fetchone()
-            message = row[0]
+            if channel_id is not None:
+                cur = await db.execute("SELECT goodbye_message FROM guilds WHERE guild_id = ?", (guild_id,))
+                row = await cur.fetchone()
+                message = row[0]
+                if message is None:
+                    message = f"{member.mention} has just left {guild.name}!"
+                color = member.accent_color
+                time = datetime.now()
+                embed = discord.Embed(color=color, description=message, timestamp=time)
+                avatar = member.display_avatar
+                embed.set_thumbnail(url=avatar)
+                channel = guild.get_channel(channel_id)
+                content = f"-# {member.mention}"
+                await channel.send(content=content, embed=embed)
             await db.commit()
             await db.close()
-        if channel_id is not None:
-            channel = guild.get_channel(channel_id)
-            if message is not None:
-                embed = discord.Embed(color=member.accent_color, description=message, timestamp=datetime.now())
-                await channel.send(embed=embed)
-            else:
-                description = f"{member.mention} has just left {guild.name}!"
-                embed = discord.Embed(color=member.accent_color, description=description, timestamp=datetime.now())
-                await channel.send(embed=embed)
 
 async def setup(bot: commands.Bot):
 	await bot.add_cog(BackgroundTasks(bot), override=True)
