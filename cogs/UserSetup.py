c4cfc63700c0e119512f419f40b1814d98bdf97c
gemhue <laceyjoms@gmail.com>
2024-10-21 17:07:28 -0500

    Editing: UserSetup

--- cogs/UserSetup.py
+++ cogs/UserSetup.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta, timezone
 class UserSetup(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.blurple = discord.Colour.blurple()
 
     @commands.hybrid_group(name="setup", fallback="channels")
     @commands.has_guild_permissions(administrator=True)
@@ -24,10 +25,12 @@ class UserSetup(commands.Cog):
         goodbye_channel : discord.TextChannel, optional
             Set the channel for goodbye messages.
         """
-        guild = ctx.guild
-        guild_id = guild.id
-        embed = discord.Embed(color=ctx.author.accent_color, title="Channels Set")
+        ctx.defer(ephemeral=True)
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = ctx.guild
+            guild_id = guild.id
+            now = datetime.now(tz=timezone.utc)
+            embed = discord.Embed(color=self.blurple, title="Channels Set", timestamp=now)
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             if logging_channel is not None:
                 logging_id = logging_channel.id
@@ -53,9 +56,16 @@ class UserSetup(commands.Cog):
                 fetched_goodbye = row[0]
                 goodbye = guild.get_channel(fetched_goodbye)
                 embed.add_field(name="Goodbye Channel", value=f"{goodbye.mention}", inline=False)
+            await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+            cur = await db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                logging = self.bot.get_channel(fetched_logging)
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+                await logging.send(embed=embed)
             await db.commit()
             await db.close()
-        await ctx.send(embed=embed, ephemeral=True)
 
     @setup.command(name="welcome_message")
     @commands.has_guild_permissions(administrator=True)
@@ -69,19 +79,27 @@ class UserSetup(commands.Cog):
             Set the welcome message for members who join the server.
         """
         ctx.defer(ephemeral=True)
-        guild = ctx.guild
-        guild_id = guild.id
-        embed = discord.Embed(color=ctx.author.accent_color, title="Message Set")
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = ctx.guild
+            guild_id = guild.id
+            now = datetime.now(tz=timezone.utc)
+            embed = discord.Embed(color=self.blurple, title="Message Set", timestamp=now)
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             await db.execute("UPDATE guilds SET welcome_message = ? WHERE guild_id = ?", (message, guild_id))
             cur = await db.execute("SELECT welcome_message FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             fetched_message = row[0]
             embed.add_field(name="Welcome Message", value=f"{fetched_message}", inline=False)
+            await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+            cur = await db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                logging = self.bot.get_channel(fetched_logging)
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+                await logging.send(embed=embed)
             await db.commit()
             await db.close()
-        await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
 
     @setup.command(name="goodbye_message")
     @commands.has_guild_permissions(administrator=True)
@@ -95,19 +113,27 @@ class UserSetup(commands.Cog):
             Set the goodbye message for members who leave the server.
         """
         ctx.defer(ephemeral=True)
-        guild = ctx.guild
-        guild_id = guild.id
-        embed = discord.Embed(color=ctx.author.accent_color, title="Message Set")
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = ctx.guild
+            guild_id = guild.id
+            now = datetime.now(tz=timezone.utc)
+            embed = discord.Embed(color=self.blurple, title="Message Set", timestamp=now)
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             await db.execute("UPDATE guilds SET goodbye_message = ? WHERE guild_id = ?", (message, guild_id))
             cur = await db.execute("SELECT goodbye_message FROM guilds WHERE guild_id = ?", (guild_id,))
             row = await cur.fetchone()
             fetched_message = row[0]
             embed.add_field(name="Goodbye Message", value=f"{fetched_message}", inline=False)
+            await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+            cur = await db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                logging = self.bot.get_channel(fetched_logging)
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+                await logging.send(embed=embed)
             await db.commit()
             await db.close()
-        await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
 
     @setup.command(name="join_roles")
     @commands.has_guild_permissions(administrator=True)
@@ -123,10 +149,11 @@ class UserSetup(commands.Cog):
             Choose the role that you would like to give to new bots on join.
         """
         ctx.defer(ephemeral=True)
-        guild = ctx.guild
-        guild_id = guild.id
-        embed = discord.Embed(color=ctx.author.accent_color, title="Roles Set")
         async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = ctx.guild
+            guild_id = guild.id
+            now = datetime.now(tz=timezone.utc)
+            embed = discord.Embed(color=self.blurple, title="Roles Set", timestamp=now)
             await db.execute("INSERT OR IGNORE INTO guilds (guild_id) VALUES (?)", (guild_id,))
             role_id = role.id
             await db.execute("UPDATE guilds SET join_role_id = ? WHERE guild_id = ?", (role_id, guild_id))
@@ -143,9 +170,16 @@ class UserSetup(commands.Cog):
                 botrole_id = row[0]
                 botrole = guild.get_role(botrole_id)
                 embed.add_field(name="Bot Join Role", value=f"{botrole.mention}", inline=False)
-            await db.commit
+            await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+            cur = await db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                logging = self.bot.get_channel(fetched_logging)
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+                await logging.send(embed=embed)
+            await db.commit()
             await db.close()
-        ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
 
     @setup.command(name="activity_roles")
     @commands.has_guild_permissions(administrator=True)
@@ -163,38 +197,49 @@ class UserSetup(commands.Cog):
             Choose the role that you would like to give to active members.
         """
         await ctx.defer(ephemeral=True)
-        guild = ctx.guild
-        channels = guild.text_channels
-        members = [m for m in guild.members if not m.bot]
-        today =  datetime.now(timezone.utc)
-        setdays = timedelta(days=days)
-        daysago = today-setdays
-        newmembers = [m for m in members if m.joined_at < daysago]
-        activemembers = []
-        inactivemembers = []
-        for channel in channels:
-            async for message in channel.history(after=daysago):
-                if message.author in members and message.author not in activemembers:
-                    activemembers.append(message.author)
-        for member in members:
-            if member not in newmembers and member not in activemembers:
-                inactivemembers.append(member)
-            if member in newmembers and member not in activemembers:
-                activemembers.append(member)
-        for member in activemembers:
-            if active not in member.roles:
-                await member.add_roles(active)
-            if inactive in member.roles:
-                await member.remove_roles(inactive)
-        for member in inactivemembers:
-            if inactive not in member.roles:
-                await member.add_roles(inactive)
-            if active in member.roles:
-                await member.remove_roles(active)
-        embed = discord.Embed(color=ctx.author.accent_color, title="Activity Roles Assigned")
-        embed.add_field(name="Active Members", value=f"{len(activemembers)} members now have the {active.mention} role!", inline=False)
-        embed.add_field(name="Inactive Members", value=f"{len(inactivemembers)} members now have the {inactive.mention} role!", inline=False)
-        await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+        async with aiosqlite.connect('rainbowbot.db') as db:
+            guild = ctx.guild
+            guild_id = guild.id
+            channels = guild.text_channels
+            members = [m for m in guild.members if not m.bot]
+            today =  datetime.now(tz=timezone.utc)
+            setdays = timedelta(days=days)
+            daysago = today-setdays
+            newmembers = [m for m in members if m.joined_at < daysago]
+            activemembers = []
+            inactivemembers = []
+            for channel in channels:
+                async for message in channel.history(after=daysago):
+                    if message.author in members and message.author not in activemembers:
+                        activemembers.append(message.author)
+            for member in members:
+                if member not in newmembers and member not in activemembers:
+                    inactivemembers.append(member)
+                if member in newmembers and member not in activemembers:
+                    activemembers.append(member)
+            for member in activemembers:
+                if active not in member.roles:
+                    await member.add_roles(active)
+                if inactive in member.roles:
+                    await member.remove_roles(inactive)
+            for member in inactivemembers:
+                if inactive not in member.roles:
+                    await member.add_roles(inactive)
+                if active in member.roles:
+                    await member.remove_roles(active)
+            embed = discord.Embed(color=self.blurple, title="Activity Roles Assigned", timestamp=today)
+            embed.add_field(name="Active Members", value=f"{len(activemembers)} members now have the {active.mention} role!", inline=False)
+            embed.add_field(name="Inactive Members", value=f"{len(inactivemembers)} members now have the {inactive.mention} role!", inline=False)
+            await ctx.send(embed=embed, delete_after=30.0, ephemeral=True)
+            cur = await db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (guild_id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                logging = self.bot.get_channel(fetched_logging)
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+                await logging.send(embed=embed)
+            await db.commit()
+            await db.close()
 
 async def setup(bot: commands.Bot):
 	await bot.add_cog(UserSetup(bot), override=True)
