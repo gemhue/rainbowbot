72cf252a52b6e7e426cca1050f5154b6058b46c9
gemhue <laceyjoms@gmail.com>
2024-10-25 21:53:07 -0500

    Editing: UserSetup

--- cogs/UserSetup.py
+++ cogs/UserSetup.py
@@ -3,7 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 import aiosqlite
 from typing import Optional
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 class UserSetup(commands.Cog):
     def __init__(self, bot: commands.Bot):
