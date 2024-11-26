039ebf65fa897b4c8acea095be65f055efb2c1d4
gemhue <laceyjoms@gmail.com>
2024-11-26 11:42:25 -0600

    Adding Help Command

--- RainbowBot.py
+++ RainbowBot.py
@@ -131,13 +131,17 @@ class CogButtons(discord.ui.View):
         await interaction.response.defer()
         message = interaction.message
 
-        embed = discord.Embed(color=discord.Color.blurple(), title="AutoDelete", description="These commands allow you to set the messages in a channel to be automatically deleted on a rolling basis.")
-        embed.add_field(name="/autodelete start <amount> <interval>",
-                        value="(Admin Only) Sets the messages in the current channel to be autodeleted.\n\n> `amount` - Set the amount of time. The lowest possible frequency is 30 minutes.\n\n> `interval` - Set the time interval. The lowest possible frequency is 30 minutes.",
-                        inline=False)
-        embed.add_field(name="/autodelete cancel",
-                        value="(Admin Only) Cancels the autodelete set for the current channel.",
-                        inline=False)
+        embed = discord.Embed(color=discord.Color.blurple(), title="Profiles", description="These commands allow you and your server members to set up member profiles that can be viewed and edited.")
+        embed.add_field(
+            name="/profile set <name> <age> <location> <pronouns> <gender> <sexuality> <relationship_status> <family_status> <biography>",
+            value="Run this command to set up your member profile. Note that all fields are optional.\n\n> `name` - Provide your name or nickname.\n\n> `age` - Provide your age or age range.\n\n> `location` - Provide your continent, country, state, or city of residence.\n\n> `pronouns` - Provide your pronouns (ex. she/her, he/him, they/them, etc).\n\n> `gender` - Provide your gender identity label (ex. woman, man, nonbinary, etc).\n\n> `sexuality` - Provide your sexuality label (ex. lesbian, gay, bisexual, etc).\n\n> `relationship_status` - Provide your relationship status (ex. single, married, etc).\n\n> `family_status` - Provide your your family planning status (ex. TTC, expecting, parenting, etc).\n\n> `biography` - Provide a brief biography (ex. family, hobbies, interests, work, etc).",
+            inline=False
+        )
+        embed.add_field(
+            name="/profile get <member>",
+            value="Run this command to retrieve a member's profile.\n\n> `member` - Provide the member whose profile you would like to retrieve.",
+            inline=False
+        )
 
         await interaction.followup.edit_message(message_id=message.id, embed=embed, view=self)
     
@@ -146,13 +150,27 @@ class CogButtons(discord.ui.View):
         await interaction.response.defer()
         message = interaction.message
 
-        embed = discord.Embed(color=discord.Color.blurple(), title="AutoDelete", description="These commands allow you to set the messages in a channel to be automatically deleted on a rolling basis.")
-        embed.add_field(name="/autodelete start <amount> <interval>",
-                        value="(Admin Only) Sets the messages in the current channel to be autodeleted.\n\n> `amount` - Set the amount of time. The lowest possible frequency is 30 minutes.\n\n> `interval` - Set the time interval. The lowest possible frequency is 30 minutes.",
-                        inline=False)
-        embed.add_field(name="/autodelete cancel",
-                        value="(Admin Only) Cancels the autodelete set for the current channel.",
-                        inline=False)
+        embed = discord.Embed(color=discord.Color.blurple(), title="Purge", description="These commands allow you to easily mass-delete messages in a single channel or in multiple channels at once.")
+        embed.add_field(
+            name="/purge here",
+            value="(Admin Only) Purge all unpinned messages in the current channel.",
+            inline=False
+        )
+        embed.add_field(
+            name="/purge member <member>",
+            value="(Admin Only) Purge all of a member's unpinned messages in a set list of up to 25 channels.\n\n> `member` - Provide the member who's unpinned messages you would like to purge.",
+            inline=False
+        )
+        embed.add_field(
+            name="/purge channels",
+            value="(Admin Only) Purge all unpinned messages in a set list of up to 25 channels.",
+            inline=False
+        )
+        embed.add_field(
+            name="/purge server",
+            value="(Admin Only) Purges all unpinned messages in a server, excluding up to 25 channels.",
+            inline=False
+        )
 
         await interaction.followup.edit_message(message_id=message.id, embed=embed, view=self)
     
@@ -161,13 +179,27 @@ class CogButtons(discord.ui.View):
         await interaction.response.defer()
         message = interaction.message
 
-        embed = discord.Embed(color=discord.Color.blurple(), title="AutoDelete", description="These commands allow you to set the messages in a channel to be automatically deleted on a rolling basis.")
-        embed.add_field(name="/autodelete start <amount> <interval>",
-                        value="(Admin Only) Sets the messages in the current channel to be autodeleted.\n\n> `amount` - Set the amount of time. The lowest possible frequency is 30 minutes.\n\n> `interval` - Set the time interval. The lowest possible frequency is 30 minutes.",
-                        inline=False)
-        embed.add_field(name="/autodelete cancel",
-                        value="(Admin Only) Cancels the autodelete set for the current channel.",
-                        inline=False)
+        embed = discord.Embed(color=discord.Color.blurple(), title="Remind", description="These commands allow you to set reminders for yourself, a user, a role, or everyone.")
+        embed.add_field(
+            name="/remind everyone <what> <when> <where>",
+            value="(Admin Only) Create a reminder for everyone.\n\n> `what` - What would you like the bot to remind everyone about?\n\n> `when` - When would you like the bot to send the reminder?\n\n> `where` - Where would you like the bot to send the reminder?",
+            inline=False
+        )
+        embed.add_field(
+            name="/remind role <who> <what> <when> <where>",
+            value="(Admin Only) Create a reminder for all users with a specified role.\n\n> `who` - Choose the role that should be sent the reminder.\n\n> `what` - What would you like the bot to remind the role about?\n\n> `when` - When would you like the bot to send the reminder?\n\n> `where` - Where would you like the bot to send the reminder?",
+            inline=False
+        )
+        embed.add_field(
+            name="/remind user <who> <what> <when> <where>",
+            value="(Admin Only) Create a reminder for a specified user.\n\n> `who` - Choose the user that should be sent the reminder.\n\n> `what` - What would you like the bot to remind the user about?\n\n> `when` - When would you like the bot to send the reminder?\n\n> `where` - Where would you like the bot to send the reminder?",
+            inline=False
+        )
+        embed.add_field(
+            name="/remind me <what> <when> <where>",
+            value="Create a reminder for yourself.\n\n> `what` - What would you like the bot to remind you about?\n\n> `when` - When would you like the bot to send the reminder?\n\n> `where` - Where would you like the bot to send the reminder?",
+            inline=False
+        )
 
         await interaction.followup.edit_message(message_id=message.id, embed=embed, view=self)
 
@@ -176,13 +208,37 @@ class CogButtons(discord.ui.View):
         await interaction.response.defer()
         message = interaction.message
 
-        embed = discord.Embed(color=discord.Color.blurple(), title="AutoDelete", description="These commands allow you to set the messages in a channel to be automatically deleted on a rolling basis.")
-        embed.add_field(name="/autodelete start <amount> <interval>",
-                        value="(Admin Only) Sets the messages in the current channel to be autodeleted.\n\n> `amount` - Set the amount of time. The lowest possible frequency is 30 minutes.\n\n> `interval` - Set the time interval. The lowest possible frequency is 30 minutes.",
-                        inline=False)
-        embed.add_field(name="/autodelete cancel",
-                        value="(Admin Only) Cancels the autodelete set for the current channel.",
-                        inline=False)
+        embed = discord.Embed(color=discord.Color.blurple(), title="RSS Feeds", description="These commands allow you to easily assign and unassign RSS feeds to Webhooks to post new entries automatically.")
+        embed.add_field(
+            name="/rss webhook_setup <webhook_nickname> <webhook_url>",
+            value="(Admin Only) Run this command to set up a Webhook for posting RSS feeds.\n\n> `webhook_nickname` - Provide a nickname for the Webhook.\n\n> `webhook_url` - Provide the URL for the Webhook.",
+            inline=False
+        )
+        embed.add_field(
+            name="/rss webhook_check <webhook_nickname>",
+            value="(Admin Only) Run this command to check what RSS feeds are set to the webhook.\n\n> `webhook_nickname` - Provide the nickname for the webhook that you would like to check.",
+            inline=False
+        )
+        embed.add_field(
+            name="/rss webhook_clear <webhook_nickname>",
+            value="(Admin Only) Run this command to clear all RSS feeds assigned to a webhook.\n\n> `webhook_nickname` - Provide the nickname for the webhook that you would like to clear.",
+            inline=False
+        )
+        embed.add_field(
+            name="/rss webhook_delete <webhook_nickname>",
+            value="(Admin Only) Run this command to delete a webhook from the database.\n\n> `webhook_nickname` - Provide the nickname for the webhook that you would like to delete.",
+            inline=False
+        )
+        embed.add_field(
+            name="/rss feed_setup <webhook_nickname> <rss_feed_url>",
+            value="(Admin Only) Run this command to set an RSS Feed. All fields are required.\n\n> `webhook_nickname` - Provide the nickname for the webhook that you are assigning the RSS feed to.\n\n> `rss_feed_url` - Provide the URL for the RSS feed that you are assigning to the webhook.",
+            inline=False
+        )
+        embed.add_field(
+            name="/rss feed_clear <webhook_nickname> <rss_feed_position>",
+            value="(Admin Only) Run this command to clear one RSS feed from a webhook.\n\n> `webhook_nickname` - Provide the nickname for the webhook that contains the RSS feed to be cleared.\n\n> `rss_feed_position` - Provide the position (0-9) on the webhook where the RSS feed to be cleared is located.",
+            inline=False
+        )
 
         await interaction.followup.edit_message(message_id=message.id, embed=embed, view=self)
 
@@ -191,13 +247,12 @@ class CogButtons(discord.ui.View):
         await interaction.response.defer()
         message = interaction.message
 
-        embed = discord.Embed(color=discord.Color.blurple(), title="AutoDelete", description="These commands allow you to set the messages in a channel to be automatically deleted on a rolling basis.")
-        embed.add_field(name="/autodelete start <amount> <interval>",
-                        value="(Admin Only) Sets the messages in the current channel to be autodeleted.\n\n> `amount` - Set the amount of time. The lowest possible frequency is 30 minutes.\n\n> `interval` - Set the time interval. The lowest possible frequency is 30 minutes.",
-                        inline=False)
-        embed.add_field(name="/autodelete cancel",
-                        value="(Admin Only) Cancels the autodelete set for the current channel.",
-                        inline=False)
+        embed = discord.Embed(color=discord.Color.blurple(), title="Tickets", description="This command allows you to set up a simple ticketing system for your server using threads.")
+        embed.add_field(
+            name="/tickets setup <channel> <staff_role>",
+            value="(Admin Only) Set up a ticketing system for the server.\n\n> `channel` - Provide the channel where the ticketing system should be posted.\n\n> `staff_role` - Provide the role that should be pinged when a ticket is opened.",
+            inline=False
+        )
 
         await interaction.followup.edit_message(message_id=message.id, embed=embed, view=self)
     
