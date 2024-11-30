7b78b93da7d1960b97267fe622f727858a704e29
gemhue <laceyjoms@gmail.com>
2024-11-30 10:55:40 -0600

    autodelete rewrite

--- cogs/autodelete.py
+++ cogs/autodelete.py
@@ -1,68 +1,67 @@
 import discord
 import traceback
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 from datetime import datetime, timedelta, timezone
-from typing import Literal
 
 class AutoDelete(commands.GroupCog, group_name="autodelete"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.db = bot.database
     
-    def cog_load(self):
-        self.autodeleter.start()
-    
-    def cog_unload(self):
-        self.autodeleter.cancel()
-    
     @app_commands.command(name="start")
     @app_commands.checks.has_permissions(administrator=True)
-    async def start(self, interaction: discord.Interaction, amount: int, interval: Literal["Minutes", "Hours", "Days"]):
+    @app_commands.choices(
+        interval = [
+            app_commands.Choice(name="1 Minute", value=1),
+            app_commands.Choice(name="15 Minutes", value=15),
+            app_commands.Choice(name="30 Minutes", value=30),
+            app_commands.Choice(name="45 Minutes", value=45),
+            app_commands.Choice(name="1 Hour", value=60),
+            app_commands.Choice(name="6 Hours", value=360),
+            app_commands.Choice(name="12 Hours", value=720),
+            app_commands.Choice(name="18 Hours", value=1080),
+            app_commands.Choice(name="1 Day", value=1440),
+            app_commands.Choice(name="1 Week", value=10080),
+            app_commands.Choice(name="2 Weeks", value=20160)
+        ]
+    )
+    async def start(self, interaction: discord.Interaction, interval: app_commands.Choice[int]):
         """(Admin Only) Sets the messages in the current channel to be autodeleted.
 
         Parameters
         -----------
-        amount : int
-            Set the amount of time. The lowest possible frequency is 30 minutes.
-        interval : str
-            Set the time interval. The lowest possible frequency is 30 minutes.
+        interval : int
+            How long should a message stay in this channel before being automatically deleted?
         """
         await interaction.response.defer(ephemeral=True)
         try:
-                # Add the guild to the database if necessary
-                await self.db.execute("INSERT OR INGORE INTO autodelete (channel_id) VALUES ?", (interaction.channel.id,))
-                await self.db.commit()
+            # Add the channel_id to the database if necessary
+            await self.db.execute("INSERT OR INGORE INTO autodelete (channel_id) VALUES ?", (interaction.channel.id,))
+            await self.db.commit()
 
-                # Update and retrieve the amount
-                await self.db.execute("UPDATE autodelete SET amount = ? WHERE channel_id = ?", (amount, interaction.channel.id))
-                await self.db.commit()
-                cur = await self.db.execute("SELECT amount FROM autodelete WHERE channel_id = ?", (interaction.channel.id,))
-                row = await cur.fetchone()
-                amount = row[0]
+            # Update and retrieve the interval
+            await self.db.execute("UPDATE autodelete SET interval = ? WHERE channel_id = ?", (interval, interaction.channel.id))
+            await self.db.commit()
+            cur = await self.db.execute("SELECT interval FROM autodelete WHERE channel_id = ?", (interaction.channel.id,))
+            row = await cur.fetchone()
+            interval = row[0]
 
-                # Update and retrieve the interval
-                await self.db.execute("UPDATE autodelete SET interval = ? WHERE channel_id = ?", (interval, interaction.channel.id))
-                await self.db.commit()
-                cur = await self.db.execute("SELECT interval FROM autodelete WHERE channel_id = ?", (interaction.channel.id,))
-                row = await cur.fetchone()
-                interval = row[0]
+            # Send a message that the AutoDelete has been set
+            embed = discord.Embed(color=self.bot.green, title="Success", description=f"The autodelete for {interaction.channel.mention} has been set up. Any unpinned messages older than **{interval} minutes** will be automatically deleted.")
+            await interaction.followup.send(embed=embed, ephemeral=True)
 
-                # Send a message that the AutoDelete has been set
-                embed = discord.Embed(color=self.bot.green, title="Success", description=f"The autodelete for {interaction.channel.mention} has been set up. Any unpinned messages older than **{amount} {interval}** will be automatically deleted on a rolling basis. Please note that if you set a time of less than 30 minutes, messages will be deleted no more frequently than every 30 minutes.")
-                await interaction.followup.send(embed=embed, ephemeral=True)
-
-                # Send a log to the logging channel
-                cur = await self.db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (interaction.guild.id,))
-                row = await cur.fetchone()
-                fetched_logging = row[0]
-                if fetched_logging is not None:
-                    now = datetime.now(tz=timezone.utc)
-                    log = discord.Embed(color=self.bot.blurple, title="AutoDelete Log", description=f"{interaction.user.mention} has just set up AutoDelete for {interaction.channel.mention}. Any unpinned messages in the channel older than **{amount} {interval}** will be automatically deleted.", timestamp=now)
-                    log.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-                    log.set_thumbnail(url=interaction.user.display_avatar)
-                    logging = self.bot.get_channel(fetched_logging)
-                    await logging.send(embed=log)
+            # Send a log to the logging channel
+            cur = await self.db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (interaction.guild.id,))
+            row = await cur.fetchone()
+            fetched_logging = row[0]
+            if fetched_logging is not None:
+                now = datetime.now(tz=timezone.utc)
+                log = discord.Embed(color=self.bot.blurple, title="AutoDelete Log", description=f"{interaction.user.mention} has just set up AutoDelete for {interaction.channel.mention}. Any unpinned messages in the channel older than **{interval} minutes** will be automatically deleted.", timestamp=now)
+                log.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
+                log.set_thumbnail(url=interaction.user.display_avatar)
+                logging = self.bot.get_channel(fetched_logging)
+                await logging.send(embed=log)
 
         # Send an error message if there is an issue
         except Exception as e:
@@ -77,38 +76,38 @@ class AutoDelete(commands.GroupCog, group_name="autodelete"):
         """
         await interaction.response.defer(ephemeral=True)
         try:
+            # Delete the channel_id from the database
+            await self.db.execute("DELETE FROM autodelete WHERE channel_id = ?", (interaction.channel.id,))
+            await self.db.commit()
 
-                await self.db.execute("DELETE FROM autodelete WHERE channel_id = ?", (interaction.channel.id,))
-                await self.db.commit()
+            # Confirm that the channel_id has been removed from autodelete
+            cur = await self.db.execute("SELECT EXISTS(SELECT 1 FROM autodelete WHERE channel_id = ?)", (interaction.channel.id,))
+            row = await cur.fetchone()
+            exists = row[0]
 
-                # Confirm that the channel_id has been removed from autodelete
-                cur = await self.db.execute("SELECT EXISTS(SELECT 1 FROM autodelete WHERE channel_id = ?)", (interaction.channel.id,))
+            if exists == 0:
+
+                # Send a message that the AutoDelete has been cancelled
+                embed = discord.Embed(color=self.bot.green, title="Success", description=f"The autodelete for {interaction.channel.mention} has been cancelled.")
+                await interaction.followup.send(embed=embed, ephemeral=True)
+
+                # Send a log to the logging channel
+                cur = await self.db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (interaction.guild.id,))
                 row = await cur.fetchone()
-                exists = row[0]
+                fetched_logging = row[0]
+                if fetched_logging is not None:
+                    now = datetime.now(tz=timezone.utc)
+                    log = discord.Embed(color=self.bot.blurple, title="AutoDelete Log", description=f"{interaction.user.mention} has just cancelled AutoDelete for {interaction.channel.mention}.", timestamp=now)
+                    log.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
+                    log.set_thumbnail(url=interaction.user.display_avatar)
+                    logging = self.bot.get_channel(fetched_logging)
+                    await logging.send(embed=log)
 
-                if exists == 0:
+            else:
 
-                    # Send a message that the AutoDelete has been cancelled
-                    embed = discord.Embed(color=self.bot.green, title="Success", description=f"The autodelete for {interaction.channel.mention} has been cancelled.")
-                    await interaction.followup.send(embed=embed, ephemeral=True)
-
-                    # Send a log to the logging channel
-                    cur = await self.db.execute("SELECT logging_channel_id FROM guilds WHERE guild_id = ?", (interaction.guild.id,))
-                    row = await cur.fetchone()
-                    fetched_logging = row[0]
-                    if fetched_logging is not None:
-                        now = datetime.now(tz=timezone.utc)
-                        log = discord.Embed(color=self.bot.blurple, title="AutoDelete Log", description=f"{interaction.user.mention} has just cancelled AutoDelete for {interaction.channel.mention}.", timestamp=now)
-                        log.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-                        log.set_thumbnail(url=interaction.user.display_avatar)
-                        logging = self.bot.get_channel(fetched_logging)
-                        await logging.send(embed=log)
-
-                else:
-
-                    # Send a message that there was an issue cancelling the AutoDelete
-                    error = discord.Embed(color=self.bot.red, title="Error", description="There was an issue cancelling the AutoDelete. Please try again later.")
-                    await interaction.followup.send(embed=error, ephemeral=True)
+                # Send a message that there was an issue cancelling the AutoDelete
+                error = discord.Embed(color=self.bot.red, title="Error", description="There was an issue cancelling the AutoDelete. Please try again later.")
+                await interaction.followup.send(embed=error, ephemeral=True)
 
         # Send an error message if there is an issue
         except Exception as e:
@@ -116,38 +115,27 @@ class AutoDelete(commands.GroupCog, group_name="autodelete"):
             await interaction.followup.send(embed=error, ephemeral=True)
             print(traceback.format_exc())
 
-    # Check for messages to AutoDelete every 30 minutes
-    @tasks.loop(minutes=30.0)
-    async def autodeleter(self):
+    @commands.Cog.listener(name="on_message")
+    async def on_message(self, message: discord.Message):
         try:
+            channel = message.channel
             ids = []
             cur = await self.db.execute("SELECT channel_id FROM autodelete")
             rows = await cur.fetchall()
             for row in rows:
                 ids.append(row[0])
             for id in ids:
-                channel = await self.bot.fetch_channel(id)
-                if channel is not None:
-                    now = datetime.now(tz=timezone.utc)
-                    cur = await self.db.execute("SELECT amount FROM autodelete WHERE channel_id = ?", (id,))
-                    row = await cur.fetchone()
-                    amount = row[0]
+                if id == channel.id:
                     cur = await self.db.execute("SELECT interval FROM autodelete WHERE channel_id = ?", (id,))
                     row = await cur.fetchone()
                     interval = row[0]
-                    if interval == "Seconds":
-                        time = timedelta(seconds=amount)
-                    elif interval == "Minutes":
-                        time = timedelta(minutes=amount)
-                    elif interval == "Hours":
-                        time = timedelta(hours=amount)
-                    elif interval == "Days":
-                        time = timedelta(days=amount)
-                    timeago = now-time
-                    messages = [m async for m in channel.history(limit=None, before=timeago)]
-                    for message in messages:
-                        if not message.pinned:
-                            await message.delete()
+                    if interval is not None:
+                        minutes = timedelta(minutes=interval)
+                        now = datetime.now(tz=timezone.utc)
+                        delete_time = now + minutes
+                        await discord.utils.sleep_until(delete_time)
+                        await message.delete()
+        
         except Exception:
             traceback.print_exc()
 
