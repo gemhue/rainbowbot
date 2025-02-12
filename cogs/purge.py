00f88b9efc14370fdacc5555d1e21fc9935d833a
gemhue <laceyjoms@gmail.com>
2025-02-12 16:20:08 -0600

    Debugging

--- cogs/purge.py
+++ cogs/purge.py
@@ -137,7 +137,7 @@ class Purge(commands.GroupCog, group_name = "purge"):
                 )
                 await response.edit(embed=wait, view=None)
                 time = datetime.now(tz=timezone.utc) - timedelta(weeks=2.0)
-                messages = [m async for m in await channel.history()]
+                messages = [m async for m in channel.history()]
                 limit = len(messages)
                 deleted = await channel.purge(limit=limit, check=lambda m: m.pinned == False, after=time, oldest_first=True, bulk=True)
                 if len(deleted) == 1:
@@ -255,7 +255,7 @@ class Purge(commands.GroupCog, group_name = "purge"):
                     await response.edit(embed=wait, view=None)
                     time = datetime.now(tz=timezone.utc) - timedelta(weeks=2.0)
                     for channel in csv.channels:
-                        messages = [m async for m in await channel.history()]
+                        messages = [m async for m in channel.history()]
                         limit = len(messages)
                         deleted = await channel.purge(limit=limit, check=lambda m: m.pinned == False and m.author == member, after=time, oldest_first=True, bulk=True)
                         if len(deleted) == 1:
@@ -391,7 +391,7 @@ class Purge(commands.GroupCog, group_name = "purge"):
                     await response.edit(embed=wait, view=None)
                     time = datetime.now(tz=timezone.utc) - timedelta(weeks=2.0)
                     for channel in csv.channels:
-                        messages = [m async for m in await channel.history()]
+                        messages = [m async for m in channel.history()]
                         limit = len(messages)
                         deleted = await channel.purge(limit=limit, check=lambda m: m.pinned == False and m.author == member, after=time, oldest_first=True, bulk=True)
                         if len(deleted) == 1:
@@ -522,7 +522,7 @@ class Purge(commands.GroupCog, group_name = "purge"):
                     await response.edit(embed=wait, view=None)
                     time = datetime.now(tz=timezone.utc) - timedelta(weeks=2.0)
                     for channel in csv.channels:
-                        messages = [m async for m in await channel.history()]
+                        messages = [m async for m in channel.history()]
                         limit = len(messages)
                         deleted = await channel.purge(limit=limit, check=lambda m: m.pinned == False, after=time, oldest_first=True, bulk=True)
                         if len(deleted) == 1:
@@ -654,7 +654,7 @@ class Purge(commands.GroupCog, group_name = "purge"):
                     time = datetime.now(tz=timezone.utc) - timedelta(weeks=2.0)
                     purge_channels = [c for c in guild.text_channels if c not in csv.channels]
                     for channel in purge_channels:
-                        messages = [m async for m in await channel.history()]
+                        messages = [m async for m in channel.history()]
                         limit = len(messages)
                         deleted = await channel.purge(limit=limit, check=lambda m: m.pinned == False, after=time, oldest_first=True, bulk=True)
                         if len(deleted) == 1:
